5c7850768de1a601dbc023c1db6ec24e350088e4
Taniguchi <tani_sales@hotmail.com>
2022-02-27 21:43:23 -0300

    Adicionar arquivos de projeto.

---
+++ RestWithASPNETUdemy/Controllers/PersonController.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using RestWithASPNETUdemy.Model;
+using RestWithASPNETUdemy.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestWithASPNETUdemy.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class PersonController : ControllerBase
+    {
+
+
+        private readonly ILogger<PersonController> _logger;
+
+        //Declaration of the servide used
+        private IPersonService _personService;
+
+        //Injection of an instance of IPersonService 
+        //when creating a instance of PersonController
+        public PersonController(ILogger<PersonController> logger, IPersonService personService)
+        {
+            _logger = logger;
+            _personService = personService;
+        }
+
+        //Maps GET requests to https://localhost:{port}/api/person
+        //Get no parameters for FindAll -> Search All
+        [HttpGet]
+        public IActionResult Get(string firstNumber, string secondNumber)
+        {
+            return Ok(_personService.FindAll());
+        }
+
+        // Maps GET requests to https://localhost:{port}/api/person/{id}
+        // receiving an ID as in the Request Path
+        //Get with parameters for FindById -> Search by ID
+        [HttpGet("{id}")]
+        public IActionResult Get(long id)
+        {
+            var person = _personService.FindByID(id);
+            if (person == null) return NotFound();
+            return Ok(person);
+        }
+
+        //Maps PUT requests to https://localhost:{port}/api/person/
+        //[FromBody] consumes the JSON object sent in the request body
+        [HttpPost]
+        public IActionResult Post([FromBody] Person person)
+        {
+
+            if (person == null) return BadRequest();
+            return Ok(_personService.Create(person));
+        }
+
+        //Maps PUT requests to https://localhost:{port}/api/person/
+        //[FromBody] consumes the JSON object sent in the request body
+        [HttpPut]
+        public IActionResult Put([FromBody] Person person)
+        {
+
+            if (person == null) return BadRequest();
+            return Ok(_personService.Update(person));
+        }
+
+        //Maps DELETE requests to https://localhost:{port}/api/person/{id}
+        //receiving an ID as in the Request Path
+        [HttpDelete("{id}")]
+        public IActionResult Delete(long id)
+        {
+            var person = _personService.FindByID(id);
+            if (person == null) return NotFound();
+            return NoContent();
+        }
+    }
+}
---
+++ RestWithASPNETUdemy/Services/Implementations/PersonServiceImplementation.cs
@@ -0,0 +1,101 @@
+﻿using RestWithASPNETUdemy.Model;
+using RestWithASPNETUdemy.Model.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+
+namespace RestWithASPNETUdemy.Services.Implementations
+{
+    public class PersonServiceImplementation : IPersonService
+    {
+
+       
+        private MySQLContext _context;
+
+       public PersonServiceImplementation(MySQLContext context)
+        {
+            _context = context;
+        }
+
+
+        //Method responsible for returning all people,
+        //againd thid information is mocks
+        public List<Person> FindAll()
+        {
+            return _context.Persons.ToList();
+        }
+
+
+        //Method responsible for returning a person
+        //as we have not acessed any database we are returning a mock
+        public Person FindByID(long id)
+        {
+            return _context.Persons.SingleOrDefault(p => p.Id == id);
+        } 
+        
+        public Person Create(Person person)
+        {
+            try
+            {
+                _context.Add(person);
+                _context.SaveChanges();
+
+            }
+            catch (Exception){
+
+                throw;
+            }
+            return person;
+        }
+        
+        //Method responsible for updating a person for
+        //being mock we return the same information passed
+        public Person Update(Person person)
+        {
+            if (!Exists(person.Id)) return new Person();
+
+            var result = _context.Persons.SingleOrDefault(p => p.Id.Equals(person.Id));
+            if(result != null)
+            {
+                try
+                {
+                    _context.Entry(result).CurrentValues.SetValues(person);
+                    _context.SaveChanges();
+                }
+                catch (Exception)
+                {
+                    throw;
+                }
+
+            }
+          
+            return person;
+        }
+
+        //Method responsible for deleting a person from an ID
+        public void Delete(long id)
+        {
+            var result = _context.Persons.SingleOrDefault(p => p.Id.Equals(id));
+            if(result != null)
+            {
+                try
+                {
+                    _context.Persons.Remove(result);
+                    _context.SaveChanges();
+                }
+                catch (Exception)
+                {
+                    throw;
+                }
+            }
+
+        }
+
+        private bool Exists(long id)
+        {
+            return _context.Persons.Any(p => p.Id.Equals(id));
+        }
+    }
+}
+
