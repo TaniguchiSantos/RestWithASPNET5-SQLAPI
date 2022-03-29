28423157a17dc92372eeb07dec6b3c400b4a7bca
Taniguchi <tani_sales@hotmail.com>
2022-03-29 10:55:20 -0300

    Authentication

--- RestWithASPNETUdemy/Controllers/BookController.cs
+++ RestWithASPNETUdemy/Controllers/BookController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using RestWithASPNETUdemy.Business;
 using RestWithASPNETUdemy.Hypermedia.Filters;
@@ -13,6 +14,7 @@ namespace RestWithASPNETUdemy.Controllers
     //[controller] = book
     [ApiVersion("1")]
     [ApiController]
+    [Authorize("Bearer")]
     [Route("api/[controller]/v{version:apiVersion}")]
     public class BookController : ControllerBase
     {
