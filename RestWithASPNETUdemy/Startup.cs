3f058e1e5902cfeb60ed0bb153ecc07294c16eb4
Taniguchi <tani_sales@hotmail.com>
2022-03-30 15:32:25 -0300

    Download and Upload

--- RestWithASPNETUdemy/Startup.cs
+++ RestWithASPNETUdemy/Startup.cs
@@ -30,6 +30,8 @@ using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.IdentityModel.Tokens;
 using System.Text;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Microsoft.AspNetCore.Http;
 
 namespace RestWithASPNETUdemy
 {
@@ -142,9 +144,14 @@ namespace RestWithASPNETUdemy
             });
 
             //Dependency Injection
+
+            services.TryAddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+
+
             services.AddScoped<IPersonBusiness, PersonBusinessImplementation>();
             services.AddScoped<IBookBusiness, BookBusinessImplementation>();
             services.AddScoped<ILoginBusiness, LoginBusinessImplementation>();
+            services.AddScoped<IFileBusiness, FileBusinessImplementation>();
 
             services.AddTransient<ITokenService, TokenService>();
 
